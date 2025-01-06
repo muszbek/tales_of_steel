c1b3a702d127f19d0991210e75b6a92ff6787a94
Tamás Muszbek <muszbektamas@gmail.com>
2025-01-06 19:20:56 +0100

    release with music

--- screens/ToBeConinued.gd
+++ screens/ToBeConinued.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-#const TITLE = "res://assets/music/chant_2.mp3"
+const TITLE = "res://assets/music/end_ambient_1.mp3"
 
 func _ready():
 	var music = get_tree().get_nodes_in_group("music")[-1]
-	#music.play_soundtrack(TITLE)
+	music.play_soundtrack(TITLE)
